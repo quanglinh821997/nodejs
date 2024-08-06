52604b43650e9039e9ae7625d9448773b061cb22
LeThanhTung[Sotatek] <t2403509@partner.coway.com>
2024-08-06 15:52:16 +0700

    add connection

--- src/server.js
+++ src/server.js
@@ -7,12 +7,30 @@ const webRoute = require('./routes/web')
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
 const hostname = process.env.HOST_NAME;
+const mysql = require('mysql2')
 
 // config template engine
 configViewEngine(app);
 
 //khai báo route
 app.use('/', webRoute);
+
+// test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'hoidanit',
+    port: 3307,
+    password: '123456'
+});
+
+connection.query(
+    'Select * from Users ;',
+    function (err, result, fields) {
+        console.log(">>> results= ", result);
+        console.log(">>> fields= ", fields);
+    }
+)
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
