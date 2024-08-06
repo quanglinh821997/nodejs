dd1f332552129e1e943ae71dc1b0858ff0513adf
LeThanhTung[Sotatek] <t2403509@partner.coway.com>
2024-08-06 17:55:17 +0700

    add file

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,21 +1,7 @@
 const connection = require('../config/database')
 const getHomePage = (req, res) => {
     // xu ly data
-
-
-    let user = []
-    connection.query(
-        'Select * from Users ;',
-        function (err, result, fields) {
-            user = result
-            console.log(">>> results= ", result);
-
-            console.log(">>check users: ", user)
-            res.send(JSON.stringify(user))
-        }
-    )
-
-
+    return res.render('home.ejs')
 }
 
 const getXYZ = (req, res) => {
